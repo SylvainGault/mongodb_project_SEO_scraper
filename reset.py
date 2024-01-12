c2bd25149266118c17f8f7fbff2f6dfdef7cbb9d
Sylvain Gault <github_seo@sylvaingault.fr>
2024-01-12 23:06:48 +0100

    reset: add indices
    Signed-off-by: Sylvain Gault <github_seo@sylvaingault.fr>

--- reset.py
+++ reset.py
@@ -10,6 +10,16 @@ def main():
     client = pymongo.MongoClient("mongodb://localhost:27017/")
     client.drop_database("seo")
 
+    db = client["seo"]
+    coll_urls = db["urls"]
+    coll_docs = db["docs"]
+    coll_logs = db["logs"]
+
+    coll_logs.create_index([("date", 1)])
+    coll_docs.create_index([("url", 1), ("scope", 1), ("fetch_date", 1)])
+    coll_urls.create_index([("status", "hashed")])
+    coll_urls.create_index([("url", 1), ("scope", 1)])
+
 
 
 if __name__ == "__main__":
